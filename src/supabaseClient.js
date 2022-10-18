95f484642c3542091411aad1a1bbd08052fecdf2
Tassia Forasteiro <tassiaforasteiro@gmail.com>
2022-10-18 16:17:03 +0000

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = __api.env.SVELTE_APP_SUPABASE_URL
-const supabaseAnonKey = __api.env.SVELTE_APP_SUPABASE_ANON_KEY
+const supabaseUrl = "https://fdoqunspsohmtldrwsud.supabase.co"
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZkb3F1bnNwc29obXRsZHJ3c3VkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjYwOTU0ODksImV4cCI6MTk4MTY3MTQ4OX0.AvdGJbXhoLmQsCmMHgiTrQ8tb4OsyiQyGVR3x26hwEs'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
